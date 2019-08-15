e472ed836f2b9c43dbb71dabb48e1425ba3295d5
Alan Turing <freemasl.sf@gmail.com>
2019-08-15 15:12:57 -0600

    Add removeIdea test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -19,4 +19,29 @@ describe('App', () => {
     //Expectation
     expect(wrapper.state('ideas')).toEqual(expected);
   })
+
+  it('should update state with one less idea when removeIdea is called', () => {
+    //Setup
+    const wrapper = shallow(<App />);
+    const mockIdeas = [{
+      id: 1,
+      title: 'Blah',
+      description: 'Blah blah blah'
+    },
+    {
+      id: 2,
+      title: 'Blah',
+      description: 'Blah blah blah'
+    }]
+    const filteredIdeas = [{
+      id: 2,
+      title: 'Blah',
+      description: 'Blah blah blah'
+    }]
+
+    wrapper.setState({ideas: mockIdeas})
+
+    wrapper.instance().removeIdea(1);
+    expect(wrapper.state('ideas')).toEqual(filteredIdeas)
+  })
 })
