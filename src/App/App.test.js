c7433fb5268b1772685a881925ce1b54f4e627e5
Alan Turing <freemasl.sf@gmail.com>
2019-08-15 13:32:11 -0600

    Add testing a class-based component

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('should update state with a new idea when addIdea is called', () => {
+    //Setup
+    const wrapper = shallow(<App />);
+    const mockIdea = {
+      id: Date.now(),
+      title: 'Blah',
+      description: 'Blah blah blah',
+    };
+    const expected = [mockIdea];
+
+    //Execution
+    wrapper.instance().addIdea(mockIdea);
+
+    //Expectation
+    expect(wrapper.state('ideas')).toEqual(expected);
+  })
+})
